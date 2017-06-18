055873a492c06ab93288ea5d9b42840af8b0f34f
sanket135 <sanket@techmentation.com>
2017-06-19 04:13:14 +1000

    removing Js not required

--- index.php
+++ index.php
@@ -130,42 +130,6 @@ $contents = mb_convert_encoding($contents , 'UTF-8' , 'UTF-16LE');
             
       
         
-        </div>
-       <script>
-
-function(id){
-
-  // Get the url  that opens the modal
-var btn = document.getElementById(id);
-
-// Get the modal
-var modal = document.getElementById(id);
-
-}
-
-
-
-
-// Get the <span> element that closes the modal, giving error
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-</script>
+    </div>
     </body>
 </html>
